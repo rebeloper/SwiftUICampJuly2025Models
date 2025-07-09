5eff99666e00baa3dad8657f70a87ac314602b6b
rebeloper <therebeloper@gmail.com>
2025-07-09 17:17:27 +0300

    public

--- Sources/SwiftUICampJuly2025Models/Profile.swift
+++ Sources/SwiftUICampJuly2025Models/Profile.swift
@@ -8,32 +8,32 @@
 import SwiftUI
 import FirebaseFirestore
 
-struct Profile: Codable, Identifiable {
+public struct Profile: Codable, Identifiable {
     
-    @DocumentID var id: String?
+    @DocumentID public var id: String?
     
-    var name: String
-    var score: Int
+    public var name: String
+    public var score: Int
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case id
         case name
         case score
     }
     
-    init(name: String = "", score: Int = 0) {
+    public init(name: String = "", score: Int = 0) {
         self.name = name
         self.score = score
     }
     
-    init(from decoder: any Decoder) throws {
+    public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self._id = try container.decode(DocumentID.self, forKey: .id)
         self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
         self.score = try container.decodeIfPresent(Int.self, forKey: .score) ?? 0
     }
     
-    func encode(to encoder: any Encoder) throws {
+    public func encode(to encoder: any Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(self._id, forKey: .id)
         try container.encode(self.name, forKey: .name)
